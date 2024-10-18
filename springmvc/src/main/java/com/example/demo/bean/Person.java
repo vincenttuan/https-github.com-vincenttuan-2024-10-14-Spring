660e82f3bf14f23b93debf4097b2884b82d13f49
teacher <teacher@BE-403->
2024-10-18 21:33:56 +0800

    web

--- springmvc/src/main/java/com/example/demo/bean/Person.java
+++ springmvc/src/main/java/com/example/demo/bean/Person.java
@@ -1,5 +1,40 @@
 package com.example.demo.bean;
 
+// 根據路徑: /person?name=John&age=18&score=90.5&pass=true 來定義物件屬性
 public class Person {
+	private String name;
+	private Integer age;
+	private Double score;
+	private Boolean pass;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public Double getScore() {
+		return score;
+	}
+	public void setScore(Double score) {
+		this.score = score;
+	}
+	public Boolean getPass() {
+		return pass;
+	}
+	public void setPass(Boolean pass) {
+		this.pass = pass;
+	}
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + ", score=" + score + ", pass=" + pass + "]";
+	}
+	
 	
 }
