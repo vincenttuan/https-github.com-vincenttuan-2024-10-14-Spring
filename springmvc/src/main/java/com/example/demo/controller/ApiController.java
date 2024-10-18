18a0014377b655b25e82bdbedf5eabd5ed81db2e
teacher <teacher@BE-403->
2024-10-18 19:28:39 +0800

    web

---
+++ springmvc/src/main/java/com/example/demo/controller/ApiController.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+// 了解各種不同URL與參數的傳遞接收 
+@Controller
+@RequestMapping("/api")
+public class ApiController {
+	
+	/** 
+	 * 1.歡迎頁 
+	 * 路徑: /welcome
+	 * 路徑: /home
+	 * 網址: http://localhost:8080/api/welcome
+	 * 網址: http://localhost:8080/api/home
+	 */
+	@GetMapping(value = {"/welcome", "/home"})
+	@ResponseBody
+	public String welcome() {
+		return "Welcome";
+	}
+	
+	/**
+	 * 2. ?帶參數
+	 * 路徑: /greet?name=John&age=18
+	 * 路徑: /greet?name=Mary
+	 * 網址: http://localhost:8080/api/greet?name=John&age=18
+	 * 網址: http://localhost:8080/api/greet?name=Mary
+	 * 限制: name 參數是一定要加上的
+	 *      age 參數不一定要有(若沒有 age 參數則會給初始值 = 0)
+	 * */
+	public String greet() {
+		return null;
+	}
+	
+}
