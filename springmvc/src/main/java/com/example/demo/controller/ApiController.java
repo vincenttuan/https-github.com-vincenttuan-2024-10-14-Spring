a28f1640272ceabe21ecaefc6516a441242399f0
teacher <teacher@BE-403->
2024-10-18 21:31:31 +0800

    web

--- springmvc/src/main/java/com/example/demo/controller/ApiController.java
+++ springmvc/src/main/java/com/example/demo/controller/ApiController.java
@@ -130,6 +130,7 @@ public class ApiController {
 	 * 路徑: /person?name=Mary&age=17&score=40.5&pass=false
 	 * 網址: http://localhost:8080/api/person?name=John&age=18&score=90.5&pass=true
 	 * 網址: http://localhost:8080/api/person?name=Mary&age=17&score=40.5&pass=false
+	 * 設計一個 Person 的 bean 物件 
 	 */
 	
 	
