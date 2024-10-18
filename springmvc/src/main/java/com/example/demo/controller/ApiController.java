0cf5de9d8eb3149daae0183d5c8517754c1271cb
teacher <teacher@BE-403->
2024-10-18 19:57:19 +0800

    web

--- springmvc/src/main/java/com/example/demo/controller/ApiController.java
+++ springmvc/src/main/java/com/example/demo/controller/ApiController.java
@@ -46,5 +46,11 @@ public class ApiController {
 	 * 網址: http://localhost:8080/api/bmi?h=170.0&w=60.0
 	 * 執行結果: bmi = 20.76
 	 * */
-	
+	@GetMapping("/bmi")
+	@ResponseBody
+	public String bmi(@RequestParam(value = "h") Double h, 
+					  @RequestParam(value = "w") Double w) {
+		double bmi = w / Math.pow(h/100, 2);
+		return String.format("bmi = %.2f", bmi);
+	}
 }
