cbd07d399f0d53433626f6190c9e1e33051cdc2a
teacher <teacher@BE-403->
2024-10-25 21:33:10 +0800

    web

--- springmvc/src/main/java/com/example/demo/bean/Room.java
+++ springmvc/src/main/java/com/example/demo/bean/Room.java
@@ -2,9 +2,11 @@ package com.example.demo.bean;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Room {
 	private Integer roomId;
 	private String roomName;
