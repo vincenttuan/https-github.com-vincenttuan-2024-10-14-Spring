fc10ea4345853d297eb967fce4555a95f9d27367
teacher <teacher@BE-403->
2024-10-25 19:45:44 +0800

    web

--- springmvc/src/main/java/com/example/demo/dao/RoomDaoImpl.java
+++ springmvc/src/main/java/com/example/demo/dao/RoomDaoImpl.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
@@ -12,15 +14,18 @@ import org.springframework.stereotype.Repository;
 import com.example.demo.bean.Room;
 
 @Repository
+@PropertySource("classpath:sql.properties")
 public class RoomDaoImpl implements RoomDao {
 	
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 	
+	@Value("${room.sql.findAllRooms}")
+	private String findAllRoomsSql;
+	
 	@Override
 	public List<Room> findAllRooms() {
-		String sql = "select roomId, roomName, roomSize from room";
-		return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Room.class));
+		return jdbcTemplate.query(findAllRoomsSql, new BeanPropertyRowMapper<>(Room.class));
 	}
 
 	@Override
@@ -47,7 +52,5 @@ public class RoomDaoImpl implements RoomDao {
 		String sql = "delete from room where roomId=?";
 		jdbcTemplate.update(sql, roomId);
 	}
-	
-	
 	
 }
