be2c7ce669e8373d44f6563bfd867277b9f48fc2
teacher <teacher@BE-403->
2024-10-25 19:16:46 +0800

    web

--- springmvc/src/main/java/com/example/demo/dao/RoomDaoImpl.java
+++ springmvc/src/main/java/com/example/demo/dao/RoomDaoImpl.java
@@ -1,10 +1,51 @@
 package com.example.demo.dao;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
+import com.example.demo.bean.Room;
+
 @Repository
-public class RoomDaoImpl {
+public class RoomDaoImpl implements RoomDao {
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	@Override
+	public List<Room> findAllRooms() {
+		String sql = "select roomId, roomName, roomSize from room";
+		return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Room.class));
+	}
+
+	@Override
+	public Optional<Room> getRoomById(Integer roomId) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+	@Override
+	public void addRoom(Room room) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateRoom(Integer roomId, Room room) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteRoom(Integer roomId) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	
 	
