3c9c7af1fe8f4d9f31054e67613af877dba45ed5
teacher <teacher@BE-403->
2024-10-25 20:00:39 +0800

    web

--- springmvc/src/main/java/com/example/demo/dao/RoomDaoImpl.java
+++ springmvc/src/main/java/com/example/demo/dao/RoomDaoImpl.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.bean.Room;
