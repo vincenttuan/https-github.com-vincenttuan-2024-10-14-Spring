304d0504d863e47cb5ee596a6ecf9b500e3124b0
teacher <teacher@BE-403->
2024-10-25 20:38:13 +0800

    web

--- springmvc/src/main/java/com/example/demo/service/RoomService.java
+++ springmvc/src/main/java/com/example/demo/service/RoomService.java
@@ -1,9 +1,12 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.bean.Room;
 import com.example.demo.dao.RoomDao;
 
 @Service
@@ -12,5 +15,14 @@ public class RoomService {
 	@Autowired
 	//@Qualifier("roomDaoImpl")
 	private RoomDao roomDao;
+	
+	public List<Room> getAllRooms() {
+		return roomDao.findAllRooms();
+	}
+	
+	public Room getRoomById(Integer roomId) {
+		return roomDao.getRoomById(roomId)
+					  .orElseThrow(() -> new RuntimeException("找不到會議室: roomId " + roomId));
+	}
 	
 }
