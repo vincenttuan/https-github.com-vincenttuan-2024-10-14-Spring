4f21b2aa1fbb8c78c284722b3f310c02a3de891f
teacher <teacher@BE-403->
2024-10-25 19:02:29 +0800

    web

---
+++ springmvc/src/main/java/com/example/demo/dao/RoomDao.java
@@ -0,0 +1,14 @@
+package com.example.demo.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.demo.bean.Room;
+
+public interface RoomDao {
+	List<Room> findAllRooms();
+	Optional<Room> getRoomById(Integer roomId);
+	void addRoom(Room room);
+	void updateRoom(Integer roomId, Room room);
+	void deleteRoom(Integer roomId);
+}
