5f4496977fd222b2a9a5ae95ce25fc06c58ef88d
teacher <teacher@BE-403->
2024-10-25 20:48:54 +0800

    web

--- springmvc/src/main/java/com/example/demo/service/RoomService.java
+++ springmvc/src/main/java/com/example/demo/service/RoomService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.demo.bean.Room;
 import com.example.demo.dao.RoomDao;
+import com.example.demo.exception.RoomAlreadyExistsException;
 import com.example.demo.exception.RoomNotFoundException;
 
 @Service
@@ -24,6 +26,16 @@ public class RoomService {
 	public Room getRoomById(Integer roomId) {
 		return roomDao.getRoomById(roomId)
 					  .orElseThrow(() -> new RoomNotFoundException("找不到會議室: roomId " + roomId));
+	}
+	
+	public void addRoom(Integer roomId, String roomName, Integer roomSize) {
+		// 該 Room 是否已經存在
+		Optional<Room> existingRoom = roomDao.getRoomById(roomId);
+		if(existingRoom.isPresent()) {
+			throw new RoomAlreadyExistsException("新增失敗: " + roomId + " 會議室已存在");
+		}
+		Room room = new Room(roomId, roomName, roomSize);
+		roomDao.addRoom(room);
 	}
 	
 }
