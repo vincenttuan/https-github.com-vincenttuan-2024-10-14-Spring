7f222d3c8b173bb7af131790f1ab9df2e3186dd1
teacher <teacher@BE-403->
2024-11-01 19:52:58 +0800

    web

--- springmvc/src/main/java/com/example/demo/controller/BookingRestController.java
+++ springmvc/src/main/java/com/example/demo/controller/BookingRestController.java
@@ -1,14 +1,20 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.bean.Room;
+import com.example.demo.service.RoomService;
 
 /**
  * Rest API 設計
  * Method URI
  * ----------------------------------------------------------------
- * GET    /rooms                                查詢所有會議室(多筆)
  * GET    /room/{roomId}                        查詢指定會議室(單筆)
+ * GET    /rooms                                查詢所有會議室(多筆)
  * POST   /room           /room/add             新增會議室
  * PUT    /room/{roomId}  /room/update/{roomId} 完整修改會議室
  * PATCH  /room/{roomId}  /room/update/{roomId} 指定修改會議室(Homework)
@@ -19,5 +25,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/booking/rest")
 public class BookingRestController {
-
+	
+	@Autowired
+	private RoomService roomService;
+	
+	@GetMapping("/room/{roomId}")
+	public String getRoom(@PathVariable("roomId") Integer roomId) {
+		Room room = roomService.getRoomById(roomId);
+		return room.toString();
+	}
+	
 }
