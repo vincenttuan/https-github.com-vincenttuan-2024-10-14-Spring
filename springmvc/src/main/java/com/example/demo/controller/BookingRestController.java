3e90f180a1489bc063784babb1b9f0e2d35cefe6
teacher <teacher@BE-403->
2024-11-01 21:27:59 +0800

    web

--- springmvc/src/main/java/com/example/demo/controller/BookingRestController.java
+++ springmvc/src/main/java/com/example/demo/controller/BookingRestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -59,6 +60,12 @@ public class BookingRestController {
 		return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success("新增成功", null));
 	}
 	
+	@PutMapping(value = {"/room/{roomId}", "/room/update/{roomId}"})
+	public ResponseEntity<ApiResponse<Void>> updateRoom(@PathVariable Integer roomId, @RequestBody Room room) {
+		roomService.updateRoom(roomId, room);
+		return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("修改成功", null));
+		//return ResponseEntity.ok(ApiResponse.success("修改成功", null));
+	}
 	
 	
 	// 錯誤處理: 補捉 RoomNotFoundException, RoomAlreadyExistsException
