be4cf9008d6acd587d210916f024355a16aeb34c
teacher <teacher@BE-403->
2024-11-04 21:09:31 +0800

    web

--- springmvc-client/src/main/resources/static/js/room.js
+++ springmvc-client/src/main/resources/static/js/room.js
@@ -3,7 +3,7 @@ const roomList = document.getElementById('roomList'); // 房間列表
 const resultMessage = document.getElementById('resultMessage'); // 回應訊息
 // 新增房間 HTML DOM 元素
 const roomId = document.getElementById('roomId'); // roomId
-const roomName = document.getElementById('roomName'); // 回應訊息
+const roomName = document.getElementById('roomName'); // roomName
 const roomSize = document.getElementById('roomSize'); // roomSize
 
 // 取得所有房間的資料並顯示在列表中 
