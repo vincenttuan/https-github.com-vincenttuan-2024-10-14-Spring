8464493178831f5d447c585a973128b15443a6f1
teacher <teacher@BE-403->
2024-11-04 20:27:21 +0800

    web

--- springmvc/src/main/java/com/example/demo/config/ModelMapperConfig.java
+++ springmvc/src/main/java/com/example/demo/config/ModelMapperConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 public class ModelMapperConfig {
 	
 	@Bean
-	public ModelMapper modelMapper() {
+	private ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
 	
