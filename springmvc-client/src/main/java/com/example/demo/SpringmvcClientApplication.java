9b7fb3f5b5ccc8a5880fe643f231bfbd7f064651
teacher <teacher@BE-403->
2024-11-04 19:39:02 +0800

    web

---
+++ springmvc-client/src/main/java/com/example/demo/SpringmvcClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcClientApplication.class, args);
+	}
+
+}
