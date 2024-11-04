4a575ba9eee0e9956dc00bafb382602604357d3a
teacher <teacher@BE-403->
2024-11-04 20:34:03 +0800

    web

--- springmvc-client/src/main/resources/static/js/room.js
+++ springmvc-client/src/main/resources/static/js/room.js
@@ -1,9 +1,14 @@
 // 取得所有房間的資料並顯示在列表中 
-
 const fetchRooms = async() => {
 	const response = await fetch('http://localhost:8080/booking/rest/rooms');
 	const apiResponse = await response.json();
 	console.log(apiResponse);
+	displayRooms(apiResponse.data)
+};
+
+// 顯示房間列表
+const displayRooms = (rooms) => {
+	console.log(rooms);
 };
 
 // 調用 fetchRooms
