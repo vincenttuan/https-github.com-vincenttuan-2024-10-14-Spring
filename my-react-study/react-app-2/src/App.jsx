0166de82967fe0b39acb09f2a0d651aad830d0cb
teacher <teacher@BE-403->
2024-11-11 19:22:27 +0800

    web

--- my-react-study/react-app-2/src/App.jsx
+++ my-react-study/react-app-2/src/App.jsx
@@ -5,10 +5,18 @@ import './App.css'
 
 // Message form 元件, 用來輸入訊息
 const MessageForm = () => {
+  const [message, setMessage] = useState('');
+  
+  const handleSubmit = (e) => {
+    e.preventDefault(); // 移除預設的行為
+    console.log(message);
+    setMessage(''); // 表單清空
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label htmlFor='message'>Message</label>
-      <input type="text" />
+      <input type="text" id='message' value={message} onChange={(e) => setMessage(e.target.value)} />
       <button type="submit">Submit</button>
     </form>
   );
@@ -16,7 +24,16 @@ const MessageForm = () => {
 
 // Message list 元件, 用來顯示歷史訊息
 const MessageList = () => {
-
+  return (
+    <div>
+      <h2>Message List</h2>
+      <ul>
+        <li>Hello John</li>
+        <li>I'm fine</li>
+        <li>Welcome jo~</li>
+      </ul>
+    </div>
+  );
 };
 
 
@@ -25,6 +42,7 @@ function App() {
   return (
     <>
       <MessageForm />
+      <MessageList />
     </>
   )
 }
