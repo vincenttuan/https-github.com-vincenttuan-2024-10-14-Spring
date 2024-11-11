c47b943829175b6be0b5e045d167064723729737
teacher <teacher@BE-403->
2024-11-11 21:16:57 +0800

    web

--- my-react-study/react-app-3/src/components/ScoreList.jsx
+++ my-react-study/react-app-3/src/components/ScoreList.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 
  const ScoreList = () => {
+    // 紀錄及格與不及格的狀態
+    const [passing, setPassing] = useState(true);
+
     // 成績陣列
     const scores = [
         {id: 1, name: 'Alice', score: 100},
@@ -21,10 +24,15 @@ import { useState } from "react";
                 }
             </ul>
             <h1>Score Filter</h1>
-            <button>顯示及格分數</button>
-            <button>顯示不及格分數</button>
+            <button onClick={() => setPassing(true)}>顯示及格分數</button>
+            <button onClick={() => setPassing(false)}>顯示不及格分數</button>
             <ul>
-                
+                {
+                    scores.filter((score) => (passing ? score.score >= 60 : score.score < 60))
+                        .map((score) => {
+                        return <li key={score.id}>姓名: {score.name} 分數: {score.score}</li>
+                    })
+                }    
             </ul>
         </>
     )
