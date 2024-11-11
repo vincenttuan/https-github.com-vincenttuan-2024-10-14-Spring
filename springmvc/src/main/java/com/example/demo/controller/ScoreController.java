7d75cdcbd90308aa35ebdc8d5ad1b6c3b5cd0411
teacher <teacher@BE-403->
2024-11-11 21:26:46 +0800

    web

---
+++ springmvc/src/main/java/com/example/demo/controller/ScoreController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(value = "*")
+public class ScoreController {
+	
+	@GetMapping("/scores")
+	public String getScores() {
+		return """
+				[
+			        {id: 1, name: 'Alice', score: 100},
+			        {id: 2, name: 'Bob', score: 55},
+			        {id: 3, name: 'John', score: 80},
+			        {id: 4, name: 'Mary', score: 40},
+			        {id: 5, name: 'cha', score: 70}
+			    ]
+				""".trim();
+	}
+}
