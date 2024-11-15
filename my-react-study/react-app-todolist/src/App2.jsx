be650db17de186a895406c82464d04c27e3e4686
teacher <teacher@BE-403->
2024-11-15 19:04:30 +0800

    w

---
+++ my-react-study/react-app-todolist/src/App2.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  return (
+    <> 
+      <h1>My Todo List</h1>
+      <div>
+        <input type="text" />
+        <button>Add</button>
+      </div>
+      <ul>
+        <li>吃早餐</li>
+        <li>做運動</li>
+        <li>寫程式</li>
+      </ul>
+    </>
+  )
+}
+
+export default App
