c15187d9f3e47e4acfff6d1d7bf40b83bcecbea4
teacher <teacher@BE-403->
2024-11-18 21:04:39 +0800

    web

--- springboot-todolist/src/main/java/com/example/todolist/service/TodoServiceImpl.java
+++ springboot-todolist/src/main/java/com/example/todolist/service/TodoServiceImpl.java
@@ -1,19 +1,32 @@
 package com.example.todolist.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.todolist.exception.TodoNotFoundException;
 import com.example.todolist.model.dto.TodoDTO;
+import com.example.todolist.model.entity.Todo;
+import com.example.todolist.repository.TodoRepository;
 
 @Service
 public class TodoServiceImpl implements TodoService {
-
+	
+	@Autowired
+	private TodoRepository todoRepository;
+	
+	@Autowired
+	private ModelMapper modelMapper;
+	
 	@Override
 	public List<TodoDTO> getAllTodos() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Todo> todos = todoRepository.findAll();
+		return todos.stream()
+					.map(todo -> modelMapper.map(todo, TodoDTO.class))
+					.collect(Collectors.toList());
 	}
 
 	@Override
