c45a714409fd2d26690581bf3865aad371687072
teacher <teacher@BE-403->
2024-11-18 19:36:45 +0800

    web

---
+++ springboot-todolist/src/main/java/com/example/todolist/SpringbootTodolistApplication.java
@@ -0,0 +1,13 @@
+package com.example.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTodolistApplication.class, args);
+	}
+
+}
