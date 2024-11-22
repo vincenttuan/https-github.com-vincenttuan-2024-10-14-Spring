b77f36c41e0b872c9ab42e71c30f8dfd5292bda4
teacher <teacher@BE-403->
2024-11-22 20:48:46 +0800

    web

---
+++ my-react-study/react-app-todolist/src/services/todoService.js
@@ -0,0 +1,39 @@
+/**
+ * WEB API TodoList Rest CRUD
+ * ------------------------------------------------------------
+ * GET    "http://localhost:8080/todolist/"     獲取所有待辦事項
+ * POST   "http://localhost:8080/todolist/"     新增待辦事項
+ * PUT    "http://localhost:8080/todolist/{id}" 更新待辦事項
+ * DELETE "http://localhost:8080/todolist/{id}" 刪除待辦事項
+ * ------------------------------------------------------------
+ * */
+
+const BASE_URL = 'http://localhost:8080/todolist';
+
+// 獲取所有待辦事項
+export const fetchTodos = async() => {
+    const response = await fetch(BASE_URL);
+    const result = await response.json();
+    if(result.status === 200) {
+        return result.data; // 取得 data 資料並返回
+    }
+    throw new Error(result.message);
+};
+
+// 新增待辦事項
+export const addTodo = async(todo) => {
+
+
+};
+
+// 更新待辦事項
+export const updateTodo = async(updateTodo) => {
+
+
+};
+
+// 刪除待辦事項
+export const deleteTodo = async(id) => {
+
+    
+};
