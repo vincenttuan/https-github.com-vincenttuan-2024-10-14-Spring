ff5f5a3273ec79de292c70a0e9af6abfe981f72b
teacher <teacher@BE-403->
2024-11-22 21:12:09 +0800

    web

--- my-react-study/react-app-todolist/src/App7.jsx
+++ my-react-study/react-app-todolist/src/App7.jsx
@@ -3,29 +3,19 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
-
-/**
-App6.jsx 透過 useEffect 連接後端 
-語法:
-useEffect(() => {要做的事});     // 每次畫面有渲染時都會執行
-useEffect(() => {要做的事}, []); // 畫面第一次渲染時才會執行
-useEffect(() => {要做的事}, [data1]); // 當 data1 資料改變時才會執行
-useEffect(() => {要做的事}, [data1, data2, data3]); // 當 data1, data2, data3 任一資料改變時就會執行
-*/
+import { fetchTodos, addTodo, updateTodo, deleteTodo } from './services/todoService'
 
 function App() {
 
-  const [todos, setTodos] = useState([
-    {id:1, text:'吃早餐', completed:true}, 
-    {id:2, text:'做運動', completed:false}, 
-    {id:3, text:'寫程式', completed:false}, 
-    {id:4, text:'Debug', completed:true}, 
-  ]);
-
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState([]);
   const [myText, setMyText] = useState('');
 
   useEffect(() => {
-    console.log('run useEffect');
+    console.log('取得遠端 todo list 資料');
+    // 獲取代辦事項
+    //fetchTodos().then(data => setTodos(data))
+    fetchTodos().then(setTodos).catch((e) => console.error('error:', e));
   }, []);
 
 
@@ -68,7 +58,7 @@ function App() {
   return (
     <div className='container mt-5'>
       {console.log('diaplay ui')}
-      <h1 className='text-center'>My Todo List 6</h1>
+      <h1 className='text-center'>My Todo List 7</h1>
       <TodoInput myText={myText} onTextChange={onTextChange} onAdd={onAdd} />
       <TodoList todos={todos} toggleCompletion={toggleCompletion} deleteTodo={deleteTodo} />
       <div className='mt-4'>
--- my-react-study/react-app-todolist/src/main.jsx
+++ my-react-study/react-app-todolist/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App6.jsx'
+import App from './App7.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
