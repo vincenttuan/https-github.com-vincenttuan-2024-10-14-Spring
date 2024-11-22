7829e96002e5cb81e4fbf262636c82ec3045d3d1
teacher <teacher@BE-403->
2024-11-22 18:52:23 +0800

    web

--- springboot-todolist/src/main/java/com/example/todolist/controller/TodoController.java
+++ springboot-todolist/src/main/java/com/example/todolist/controller/TodoController.java
@@ -1,5 +1,17 @@
 package com.example.todolist.controller;
 
+/**
+ * WEB API
+ * ------------------------------------------
+ * servlet-path: /todolist  (@RequestMapping)
+ * ------------------------------------------
+ * GET    "/"     獲取所有待辦事項
+ * POST   "/"     新增待辦事項
+ * PUT    "/{id}" 更新待辦事項
+ * DELETE "/{id}" 刪除待辦事項
+ * ------------------------------------------
+ * */
+
 public class TodoController {
 
 }
