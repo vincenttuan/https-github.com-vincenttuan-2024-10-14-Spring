26cf3f6dffb20225130a55e691f4847a8b52f5ea
teacher <teacher@BE-403->
2024-11-22 19:14:47 +0800

    web

--- springboot-todolist/src/main/java/com/example/todolist/SpringbootTodolistApplication.java
+++ springboot-todolist/src/main/java/com/example/todolist/SpringbootTodolistApplication.java
@@ -2,8 +2,10 @@ package com.example.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = {"com.example.aaa"}) // 掃描其他資料夾
 public class SpringbootTodolistApplication {
 
 	public static void main(String[] args) {
