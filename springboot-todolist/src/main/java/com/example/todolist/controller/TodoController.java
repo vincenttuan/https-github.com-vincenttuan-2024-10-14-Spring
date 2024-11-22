c31e196e63804085c1d634813e0e26945d469329
teacher <teacher@BE-403->
2024-11-22 18:59:23 +0800

    web

--- springboot-todolist/src/main/java/com/example/todolist/controller/TodoController.java
+++ springboot-todolist/src/main/java/com/example/todolist/controller/TodoController.java
@@ -1,5 +1,21 @@
 package com.example.todolist.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.todolist.model.dto.TodoDTO;
+import com.example.todolist.model.entity.Todo;
+import com.example.todolist.response.ApiResponse;
+import com.example.todolist.service.TodoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 /**
  * WEB API
  * ------------------------------------------
@@ -12,6 +28,30 @@ package com.example.todolist.controller;
  * ------------------------------------------
  * */
 
+@RestController
+@RequestMapping("/todolist")
+@CrossOrigin(origins = "http://localhost:5173") // 跨域請求
 public class TodoController {
-
+	
+	@Autowired
+	private TodoService todoService;
+	
+	// 獲取所有待辦事項
+	@GetMapping
+	public ResponseEntity<ApiResponse<List<TodoDTO>>> getAllTodos() {
+		List<TodoDTO> todos = todoService.getAllTodos();
+		return ResponseEntity.ok(ApiResponse.success("查詢成功", todos));
+	}
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
