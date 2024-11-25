d84d8e3148826a238ac4defa9767107bc13ca520
teacher <teacher@BE-403->
2024-11-25 18:58:58 +0800

    web

---
+++ springboot-todolist-client/src/main/java/com/example/todolist/SpringbootTodolistClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTodolistClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTodolistClientApplication.class, args);
+	}
+
+}
---
+++ springboot-todolist-client/src/test/java/com/example/todolist/SpringbootTodolistClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTodolistClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
