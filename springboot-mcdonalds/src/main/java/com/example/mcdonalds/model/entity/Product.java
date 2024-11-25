48594731cc860e00e1035e944d3480f83fc02c06
teacher <teacher@BE-403->
2024-11-25 21:01:00 +0800

    web

---
+++ springboot-mcdonalds/src/main/java/com/example/mcdonalds/model/entity/Product.java
@@ -0,0 +1,26 @@
+package com.example.mcdonalds.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "product")
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column(nullable = false, columnDefinition = "default 0")
+	private Integer price;
+	
+
+}
