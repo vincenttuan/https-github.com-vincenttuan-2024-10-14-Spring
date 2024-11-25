ebdd29238f74cbb45e05422e0650ebb56a411c65
teacher <teacher@BE-403->
2024-11-25 20:40:00 +0800

    web

---
+++ springboot-mcdonalds/src/test/java/com/example/mcdonalds/SpringbootMcdonaldsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mcdonalds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMcdonaldsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
