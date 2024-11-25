cc1d64ab31e646aaef1312dbda8affb82ebcc297
teacher <teacher@BE-403->
2024-11-25 20:54:46 +0800

    web

---
+++ springboot-mcdonalds/src/main/java/com/example/mcdonalds/model/entity/User.java
@@ -0,0 +1,25 @@
+package com.example.mcdonalds.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "`user`")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(unique = true, nullable = false)
+	private String username;
+	
+	@Column(nullable = false)
+	private String password;
+	
+}
