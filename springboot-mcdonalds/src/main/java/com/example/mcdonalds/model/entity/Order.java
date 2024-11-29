3d951884a9c19635d749bc671d1b5179040628d4
teacher <teacher@BE-403->
2024-11-29 19:09:00 +0800

    web

--- springboot-mcdonalds/src/main/java/com/example/mcdonalds/model/entity/Order.java
+++ springboot-mcdonalds/src/main/java/com/example/mcdonalds/model/entity/Order.java
@@ -1,6 +1,9 @@
 package com.example.mcdonalds.model.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -9,4 +12,27 @@ import lombok.Data;
 @Table(name = "`order`")
 public class Order {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; // 訂單 id
+	
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- springboot-mcdonalds/src/main/java/com/example/mcdonalds/model/entity/OrderItem.java
+++ springboot-mcdonalds/src/main/java/com/example/mcdonalds/model/entity/OrderItem.java
@@ -1,6 +1,10 @@
 package com.example.mcdonalds.model.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -9,4 +13,33 @@ import lombok.Data;
 @Table(name = "order_item")
 public class OrderItem {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; // 訂單項目 id
+	
+	@Column
+	private Integer quantity; // 商品數量 
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
