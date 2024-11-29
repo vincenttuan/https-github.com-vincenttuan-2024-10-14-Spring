de1ef9ba018b6708084af6e8fe651006e8b31b04
teacher <teacher@BE-403->
2024-11-29 19:51:36 +0800

    web

---
+++ springboot-mcdonalds/src/test/java/com/example/mcdonalds/OrderTest.java
@@ -0,0 +1,13 @@
+package com.example.mcdonalds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class OrderTest {
+	
+	@Test
+	public void addOrder() {
+		
+	}
+}
