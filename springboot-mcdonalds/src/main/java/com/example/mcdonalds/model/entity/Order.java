bc365e9686a21dc0643aad591080b2d226bb0ca9
teacher <teacher@BE-403->
2024-11-29 19:05:38 +0800

    web

---
+++ springboot-mcdonalds/src/main/java/com/example/mcdonalds/model/entity/Order.java
@@ -0,0 +1,12 @@
+package com.example.mcdonalds.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "`order`")
+public class Order {
+	
+}
