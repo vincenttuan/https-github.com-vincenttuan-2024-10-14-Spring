94774fbb9d1fcc1d195d2ac6578e7a2c1311a48d
teacher <teacher@BE-403->
2024-11-29 21:18:24 +0800

    web

--- springboot-mcdonalds/src/main/java/com/example/mcdonalds/model/entity/User.java
+++ springboot-mcdonalds/src/main/java/com/example/mcdonalds/model/entity/User.java
@@ -1,12 +1,17 @@
 package com.example.mcdonalds.model.entity;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -27,4 +32,29 @@ public class User {
 	
 	@OneToMany(mappedBy = "user")
 	private List<Order> orders;
+	
+	@ManyToMany
+	@JoinTable(
+			name = "user_product", // 關聯表名稱
+			joinColumns = @JoinColumn(name="user_id"), // 用戶 id 外鍵 
+			inverseJoinColumns = @JoinColumn(name = "product_id") // 商品 id 外鍵
+	)
+	private Set<Product> favoriteProducts; // 關注的商品
+	
+	/*
+     * hashCode 方法中出現了遞迴循環，通常是因為 User 和 Product 實體之間的雙向關聯造成的，Hibernate 無法處理這種循環依賴。
+     * 所以要自行實現
+     * */
+	@Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
