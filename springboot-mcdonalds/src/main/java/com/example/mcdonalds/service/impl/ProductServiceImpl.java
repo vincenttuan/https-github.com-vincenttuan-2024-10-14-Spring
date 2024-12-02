3e6f66657ce7570a7b365e616b0af33efc44ad51
teacher <teacher@BE-403->
2024-12-02 20:52:29 +0800

    web

--- springboot-mcdonalds/src/main/java/com/example/mcdonalds/service/impl/ProductServiceImpl.java
+++ springboot-mcdonalds/src/main/java/com/example/mcdonalds/service/impl/ProductServiceImpl.java
@@ -3,14 +3,20 @@ package com.example.mcdonalds.service.impl;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.mcdonalds.model.dto.ProductDTO;
+import com.example.mcdonalds.repository.ProductRepository;
 import com.example.mcdonalds.service.ProductService;
 
 @Service
 public class ProductServiceImpl implements ProductService {
-
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	
 	@Override
 	public List<ProductDTO> getAllProducts() {
 		// TODO Auto-generated method stub
