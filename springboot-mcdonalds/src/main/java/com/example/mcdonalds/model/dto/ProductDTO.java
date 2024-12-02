d50f98f913468c96e91f2f5d0810e4813e77fa33
teacher <teacher@BE-403->
2024-12-02 19:35:56 +0800

    web

---
+++ springboot-mcdonalds/src/main/java/com/example/mcdonalds/model/dto/ProductDTO.java
@@ -0,0 +1,9 @@
+package com.example.mcdonalds.model.dto;
+
+public class ProductDTO {
+	private Long id; // 來自於 Product
+	private String name; // 來自於 Product
+	private Integer price; // 來自於 Product
+	
+	private String imageBase64; // 來自於 ProductImage 
+}
