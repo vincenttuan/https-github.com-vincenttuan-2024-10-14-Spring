be989f052f4f5a5a2195cd3c4bbbaee95d68244e
teacher <teacher@BE-403->
2024-12-02 21:47:04 +0800

    web

--- springboot-mcdonalds/src/main/java/com/example/mcdonalds/service/impl/OrderServiceImpl.java
+++ springboot-mcdonalds/src/main/java/com/example/mcdonalds/service/impl/OrderServiceImpl.java
@@ -82,15 +82,18 @@ public class OrderServiceImpl implements OrderService {
 		Order order = new Order();
 		order.setUser(user); // 設定關聯
 		
-		// 3. 儲存 order
-		orderRepository.save(order); // 非聯集操作時要加入
+		// 3. 建立訂單細目
+		List<OrderItem> items = orderItems.stream() // ...OrderItem
+						.map(orderItemDTO -> { 
+							OrderItem orderItem = modelMapper.map(orderItemDTO, OrderItem.class);
+							orderItem.setOrder(order);
+							return orderItem;
+						}) // ...orderItemDTO
+						.toList(); // List<OrderItem>
 		
-		// 4. 建立訂單項目並且逐一儲存
-		orderItems.forEach(item -> { // item 的型別是 OrderItemDTO
-			OrderItem orderItem = modelMapper.map(item, OrderItem.class); // OrderItemDTO 轉 OrderItem
-			orderItem.setOrder(order); // 設定關聯關係
-			orderItemRepository.save(orderItem); // 儲存
-		});
+		// 4. 儲存訂單
+		order.setOrderItems(items);
+		orderRepository.save(order);
 		
 		return Optional.of(modelMapper.map(order, OrderDTO.class)); // Order 轉 OrderDTO
 	}
