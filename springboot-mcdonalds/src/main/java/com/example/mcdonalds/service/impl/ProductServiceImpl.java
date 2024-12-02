f8a51af62862a5d6eb482e90517cc01c03e542d8
teacher <teacher@BE-403->
2024-12-02 21:12:51 +0800

    web

--- springboot-mcdonalds/src/main/java/com/example/mcdonalds/service/impl/ProductServiceImpl.java
+++ springboot-mcdonalds/src/main/java/com/example/mcdonalds/service/impl/ProductServiceImpl.java
@@ -3,10 +3,12 @@ package com.example.mcdonalds.service.impl;
 import java.util.List;
 import java.util.Optional;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.mcdonalds.model.dto.ProductDTO;
+import com.example.mcdonalds.model.entity.Product;
 import com.example.mcdonalds.repository.ProductRepository;
 import com.example.mcdonalds.service.ProductService;
 
@@ -16,17 +18,24 @@ public class ProductServiceImpl implements ProductService {
 	@Autowired
 	private ProductRepository productRepository;
 	
+	@Autowired
+	private ModelMapper modelMapper;
 	
 	@Override
 	public List<ProductDTO> getAllProducts() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		return productRepository.findAll()  // List<Product>
+								.stream()   // ...Product
+								.map(product -> modelMapper.map(product, ProductDTO.class)) // ...ProductDTO
+								.toList();
+	}							
 
 	@Override
 	public Optional<ProductDTO> getProductById(Long id) {
-		// TODO Auto-generated method stub
-		return Optional.empty();
+		Optional<Product> optProduct = productRepository.findById(id);
+		if(optProduct.isEmpty()) {
+			return Optional.empty();
+		}
+		return Optional.of(modelMapper.map(optProduct.get(), ProductDTO.class));
 	}
 
 	@Override
