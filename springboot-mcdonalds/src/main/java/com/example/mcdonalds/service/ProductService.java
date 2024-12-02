120dcc249cacb277d5ff57638b73d69083d69ecf
teacher <teacher@BE-403->
2024-12-02 20:06:18 +0800

    web

--- springboot-mcdonalds/src/main/java/com/example/mcdonalds/service/ProductService.java
+++ springboot-mcdonalds/src/main/java/com/example/mcdonalds/service/ProductService.java
@@ -1,5 +1,23 @@
 package com.example.mcdonalds.service;
 
-public interface ProductService {
+import java.util.List;
+import java.util.Optional;
 
+import com.example.mcdonalds.model.dto.ProductDTO;
+
+/*
+ * 功能:
+ * 1.取得所有商品
+ * 2.取得指定商品
+ * 3.新增/儲存商品
+ * */
+public interface ProductService {
+	// 取得所有商品
+	List<ProductDTO> getAllProducts();
+	
+	// 取得指定商品(根據 id)
+	Optional<ProductDTO> getProductById(Long id);
+	
+	// 新增/儲存商品
+	ProductDTO saveProduct(ProductDTO productDTO);
 }
