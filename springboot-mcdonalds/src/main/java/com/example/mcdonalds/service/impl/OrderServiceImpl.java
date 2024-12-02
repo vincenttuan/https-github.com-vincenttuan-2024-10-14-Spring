3f80c856ceddc483cba1e5a170e030f4f4478280
teacher <teacher@BE-403->
2024-12-02 21:19:52 +0800

    web

--- springboot-mcdonalds/src/main/java/com/example/mcdonalds/service/impl/OrderServiceImpl.java
+++ springboot-mcdonalds/src/main/java/com/example/mcdonalds/service/impl/OrderServiceImpl.java
@@ -3,15 +3,35 @@ package com.example.mcdonalds.service.impl;
 import java.util.List;
 import java.util.Optional;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.mcdonalds.model.dto.OrderDTO;
 import com.example.mcdonalds.model.dto.OrderItemDTO;
+import com.example.mcdonalds.repository.OrderItemRepository;
+import com.example.mcdonalds.repository.OrderRepository;
+import com.example.mcdonalds.repository.ProductRepository;
+import com.example.mcdonalds.repository.UserRepository;
 import com.example.mcdonalds.service.OrderService;
 
 @Service
 public class OrderServiceImpl implements OrderService {
-
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
+	
+	@Autowired
+	private ModelMapper modelMapper;
+	
 	@Override
 	public List<OrderDTO> findOrdersByUserId(Long userId) {
 		// TODO Auto-generated method stub
