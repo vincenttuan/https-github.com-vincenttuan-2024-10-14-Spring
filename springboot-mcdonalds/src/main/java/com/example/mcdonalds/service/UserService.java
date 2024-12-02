ea8aa3bd9952a6645f2d65cdff6a0d462d785276
teacher <teacher@BE-403->
2024-12-02 20:14:52 +0800

    web

--- springboot-mcdonalds/src/main/java/com/example/mcdonalds/service/UserService.java
+++ springboot-mcdonalds/src/main/java/com/example/mcdonalds/service/UserService.java
@@ -1,5 +1,13 @@
 package com.example.mcdonalds.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.example.mcdonalds.model.dto.FavoriteProductDTO;
+import com.example.mcdonalds.model.dto.FavoriteUserDTO;
+import com.example.mcdonalds.model.dto.LoginDTO;
+import com.example.mcdonalds.model.dto.UserDTO;
+
 /*
  * 功能:
  * 1.根據帳號來查詢使用者
@@ -11,5 +19,24 @@ package com.example.mcdonalds.service;
  * 7.移除商品關注
  * */
 public interface UserService {
+	// 1.根據帳號來查詢使用者
+	Optional<UserDTO> findByUsername(String username);
+
+	// 2.登入 login
+	Optional<UserDTO> login(LoginDTO loginDTO);
 	
+	// 3.新增/儲存使用者
+	Optional<UserDTO> saveUser(UserDTO userDTO);
+	
+	// 4.使用者關注列表(使用者關注那些商品)
+	List<FavoriteProductDTO> getFavoriteProducts();
+	
+	// 5.商品關注列表(商品被那些使用者關注)
+	List<FavoriteUserDTO> getFavoriteUsers();
+	
+	// 6.新增商品關注
+	void addFavoriteProduct(Long userId, Long productId);
+	
+	// 7.移除商品關注
+	void removeFavoriteProduct(Long userId, Long productId);
 }
