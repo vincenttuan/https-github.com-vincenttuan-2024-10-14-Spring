2ee6a43a83f245598a6b13a2120c82a50daa725f
teacher <teacher@BE-403->
2024-12-02 21:23:47 +0800

    web

--- springboot-mcdonalds/src/main/java/com/example/mcdonalds/repository/OrderRepository.java
+++ springboot-mcdonalds/src/main/java/com/example/mcdonalds/repository/OrderRepository.java
@@ -9,6 +9,6 @@ import com.example.mcdonalds.model.entity.Order;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-	// 取得該用戶的訂單紀錄
+	// 取得該用戶的訂單紀錄(JPA 會自動透過方法名稱建立 SQL 語句)
 	List<Order> findByUserId(Long userId);
 }
