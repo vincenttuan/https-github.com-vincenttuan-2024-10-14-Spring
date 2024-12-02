7f1ec4d7bd28f8020bc4a656daf73f6fe8187582
teacher <teacher@BE-403->
2024-12-02 20:11:14 +0800

    web

--- springboot-mcdonalds/src/main/java/com/example/mcdonalds/service/UserService.java
+++ springboot-mcdonalds/src/main/java/com/example/mcdonalds/service/UserService.java
@@ -1,5 +1,15 @@
 package com.example.mcdonalds.service;
 
+/*
+ * 功能:
+ * 1.根據帳號來查詢使用者
+ * 2.登入 login
+ * 3.新增/儲存使用者
+ * 4.使用者關注列表(使用者關注那些商品)
+ * 5.商品關注列表(商品被那些使用者關注)
+ * 6.新增商品關注
+ * 7.移除商品關注
+ * */
 public interface UserService {
-
+	
 }
