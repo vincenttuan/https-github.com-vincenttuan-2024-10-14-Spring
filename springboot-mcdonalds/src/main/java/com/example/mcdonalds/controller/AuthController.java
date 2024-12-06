b43f1f28d888fd2f43c5f882ed8b16445d39cc9b
teacher <teacher@BE-403->
2024-12-06 19:44:00 +0800

    web

---
+++ springboot-mcdonalds/src/main/java/com/example/mcdonalds/controller/AuthController.java
@@ -0,0 +1,14 @@
+package com.example.mcdonalds.controller;
+
+/*
+ * WEB REST API
+ * ----------------------------------
+ * Servlet-Path: /auth
+ * ----------------------------------
+ * POST /login      登入
+ * GET  /logout     登出
+ * GET  /isLoggedIn 判斷目前的連線是否有登入
+ * */
+public class AuthController {
+
+}
