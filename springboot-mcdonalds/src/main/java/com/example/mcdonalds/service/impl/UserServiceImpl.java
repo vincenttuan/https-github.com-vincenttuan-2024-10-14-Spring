d2c66f47753b7f484b1ac432443fa147c016c28e
teacher <teacher@BE-403->
2024-12-06 18:46:19 +0800

    web

--- springboot-mcdonalds/src/main/java/com/example/mcdonalds/service/impl/UserServiceImpl.java
+++ springboot-mcdonalds/src/main/java/com/example/mcdonalds/service/impl/UserServiceImpl.java
@@ -3,17 +3,30 @@ package com.example.mcdonalds.service.impl;
 import java.util.List;
 import java.util.Optional;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.mcdonalds.model.dto.FavoriteProductDTO;
 import com.example.mcdonalds.model.dto.FavoriteUserDTO;
 import com.example.mcdonalds.model.dto.LoginDTO;
 import com.example.mcdonalds.model.dto.UserDTO;
+import com.example.mcdonalds.repository.ProductRepository;
+import com.example.mcdonalds.repository.UserRepository;
 import com.example.mcdonalds.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService {
-
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	@Autowired
+	private ModelMapper modelMapper;
+	
 	@Override
 	public Optional<UserDTO> findByUsername(String username) {
 		// TODO Auto-generated method stub
