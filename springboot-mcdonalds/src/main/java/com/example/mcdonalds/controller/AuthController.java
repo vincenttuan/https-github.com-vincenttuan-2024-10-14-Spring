f8e2d60a6d5b72b09896f440d7b7920cfb022bc2
teacher <teacher@BE-403->
2024-12-06 19:53:27 +0800

    web

--- springboot-mcdonalds/src/main/java/com/example/mcdonalds/controller/AuthController.java
+++ springboot-mcdonalds/src/main/java/com/example/mcdonalds/controller/AuthController.java
@@ -1,12 +1,21 @@
 package com.example.mcdonalds.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.mcdonalds.model.dto.LoginDTO;
+import com.example.mcdonalds.model.dto.UserDTO;
+import com.example.mcdonalds.response.ApiResponse;
 import com.example.mcdonalds.service.UserService;
+
+import jakarta.servlet.http.HttpSession;
 
 /*
  * WEB REST API
@@ -25,7 +34,17 @@ public class AuthController {
 	@Autowired
 	private UserService userService;
 	
-	//@PostMapping("/login")
+	@PostMapping("/login")
+	public ResponseEntity<ApiResponse<LoginDTO>> login(@RequestBody LoginDTO loginDTO, HttpSession session) {
+		// 登入判斷
+		Optional<UserDTO> optUserDTO = userService.login(loginDTO);
+		if(optUserDTO.isEmpty()) {
+			return ResponseEntity.status(404).body(ApiResponse.error(404, "登入失敗"));
+		}
+		// 登入成功並存入到 HttpSession 中
+		session.setAttribute("userDTO", optUserDTO.get());
+		return ResponseEntity.ok(ApiResponse.success("登入成功", null));
+	}
 	
 	
 }
