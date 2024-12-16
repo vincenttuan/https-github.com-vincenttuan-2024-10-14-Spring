59632022fcec1c30abbf876c723f1ee8b6f683f3
teacher <teacher@BE-403->
2024-12-16 21:01:40 +0800

    web

--- springboot-tx/src/test/java/com/example/tx/TestBuyOneBook.java
+++ springboot-tx/src/test/java/com/example/tx/TestBuyOneBook.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.example.tx.service.BookService;
 import com.example.tx.service.BuyService;
 
 @SpringBootTest
@@ -11,14 +12,24 @@ public class TestBuyOneBook {
 	@Autowired
 	private BuyService buyService;
 	
+	@Autowired
+	private BookService bookService;
 	@Test
 	public void test() {
+		String username = "John";
+		Integer bookId = 1;
+		// 取得交易前餘額		
+		Integer walletBalance = bookService.getWalletBalance(username);
+		System.out.println(username + " 交易前餘額 = " + walletBalance);
 		try {
-			buyService.buyOneBook("John", 1);
+			buyService.buyOneBook(username, bookId);
 			System.out.println("買書成功");
 		} catch (Exception e) {
 			System.err.println(e.getMessage());
 			System.err.println("買書失敗");
 		}
+		// 取得交易後餘額
+		walletBalance = bookService.getWalletBalance(username);
+		System.out.println(username + " 交易後餘額 = " + walletBalance);
 	}
 }
