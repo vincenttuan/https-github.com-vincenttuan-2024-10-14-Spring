8778c867a6c6500a307faba9fa6d6d35cc2ffc03
teacher <teacher@BE-403->
2024-12-16 20:44:59 +0800

    web

---
+++ springboot-tx/src/main/java/com/example/tx/service/impl/BuyServiceImpl.java
@@ -0,0 +1,29 @@
+package com.example.tx.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.tx.service.BookService;
+import com.example.tx.service.BuyService;
+
+@Service // 交易服務
+public class BuyServiceImpl implements BuyService {
+	
+	@Autowired
+	private BookService bookService;
+	
+	@Override
+	public void buyOneBook(String username, Integer bookId) {
+		System.out.println(username + " 要買一本書, bookId = " + bookId);
+		// 1. 查詢書本價格
+		Integer bookPrice = bookService.getBookPrice(bookId);
+		System.out.println("bookId = " + bookId + " 價格 = " + bookPrice);
+		// 2. 減去庫存(1本)
+		System.out.println("bookId = " + bookId + ", 減去一本書的庫存");
+		// 3. 修改餘額(減去一本書的價格)
+		bookService.reduceBookAmount(bookId, bookPrice);
+		// 4. 其他處理
+		System.out.println(username + " 結帳完成 ...");
+	}
+	
+}
