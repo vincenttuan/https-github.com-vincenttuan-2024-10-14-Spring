f490fb14c8ff01b4e9c3758aea9138a283b1e9cf
teacher <teacher@BE-403->
2024-12-16 20:48:11 +0800

    web

--- springboot-tx/src/main/java/com/example/tx/service/impl/BuyServiceImpl.java
+++ springboot-tx/src/main/java/com/example/tx/service/impl/BuyServiceImpl.java
@@ -15,13 +15,17 @@ public class BuyServiceImpl implements BuyService {
 	@Override
 	public void buyOneBook(String username, Integer bookId) {
 		System.out.println(username + " 要買一本書, bookId = " + bookId);
-		// 1. 查詢書本價格
+		// 1. 查詢書本價格 - select table
 		Integer bookPrice = bookService.getBookPrice(bookId);
 		System.out.println("bookId = " + bookId + " 價格 = " + bookPrice);
-		// 2. 減去庫存(1本)
-		System.out.println("bookId = " + bookId + ", 減去一本書的庫存");
-		// 3. 修改餘額(減去一本書的價格)
+		
+		// 2. 減去庫存(1本) - update book_inventory table
 		bookService.reduceBookAmount(bookId, bookPrice);
+		System.out.println("bookId = " + bookId + ", 減去一本書的庫存");
+		
+		// 3. 修改餘額(減去一本書的價格) - update wallet table 
+		bookService.updateWalletBalacen(username, bookPrice);
+		
 		// 4. 其他處理
 		System.out.println(username + " 結帳完成 ...");
 	}
