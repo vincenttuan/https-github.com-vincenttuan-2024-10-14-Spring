76d815dc239a2e8e69c886bfcccf7b950e673eb8
teacher <teacher@BE-403->
2024-12-16 19:10:32 +0800

    web

--- springboot-tx/src/main/java/com/example/tx/entity/BookInventory.java
+++ springboot-tx/src/main/java/com/example/tx/entity/BookInventory.java
@@ -1,5 +1,19 @@
 package com.example.tx.entity;
 
-public class BookInventory {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookInventory {
+	@Id
+	private Integer bookId; // 書號
+	@Column
+	private Integer bookAmount; // 庫存數量
 }
