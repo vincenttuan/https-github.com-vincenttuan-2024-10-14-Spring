db4492633c88461360500cc3a848fa16902ec1cb
teacher <teacher@BE-403->
2024-12-16 19:40:28 +0800

    web

--- springboot-tx/src/main/java/com/example/tx/repository/WalletRepository.java
+++ springboot-tx/src/main/java/com/example/tx/repository/WalletRepository.java
@@ -1,5 +1,24 @@
 package com.example.tx.repository;
 
-public class WalletRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import com.example.tx.entity.Wallet;
+
+import jakarta.transaction.Transactional;
+
+@Repository
+public interface WalletRepository extends JpaRepository<Wallet, String> {
+	// 取得客戶餘額
+	@Query(value = "select balance from wallet where username = :username", nativeQuery = true)
+	Integer getWalletBalance(String username);
+	
+	// 更新客戶餘額(目前餘額 - bookPrice)
+	@Modifying
+	@Transactional
+	@Query(value = "update wallet set balance = balance - :bookPrice where username = :username", nativeQuery = true)
+	void updateWalletBalance(Integer bookPrice, String username);
+	
 }
