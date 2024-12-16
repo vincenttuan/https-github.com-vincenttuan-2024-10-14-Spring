6fe2b66b79dd886c504ba40b77514a8e480a3318
teacher <teacher@BE-403->
2024-12-16 20:01:42 +0800

    web

--- springboot-tx/src/test/java/com/example/tx/SpringbootTxApplicationTests.java
+++ springboot-tx/src/test/java/com/example/tx/SpringbootTxApplicationTests.java
@@ -1,5 +1,12 @@
 package com.example.tx;
 
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Random;
+import java.util.Set;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +15,19 @@ class SpringbootTxApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
+		// 俄羅斯輪盤
+		List<Integer> queue = List.of(1, 2, 4, 6, 8, 9, 12, 14, 16, 17, 28);
+		
+		Set<Integer> lucky = new LinkedHashSet<>();
+		
+		while (lucky.size() < 3) {
+			int idx = new Random().nextInt(queue.size());
+			lucky.add(queue.get(idx));
+		}
+		
+		System.out.println("幸運者:" + lucky);
+		
 	}
 
 }
