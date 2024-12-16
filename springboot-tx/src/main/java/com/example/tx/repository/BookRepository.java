f54d9faeddd4813af1b3b887c3c761aafaca2314
teacher <teacher@BE-403->
2024-12-16 19:36:10 +0800

    web

--- springboot-tx/src/main/java/com/example/tx/repository/BookRepository.java
+++ springboot-tx/src/main/java/com/example/tx/repository/BookRepository.java
@@ -1,5 +1,15 @@
 package com.example.tx.repository;
 
-public class BookRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.example.tx.entity.Book;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer> {
+	// 取得書本價格
+	@Query(value = "select book_price from book where book_id = :bookId", nativeQuery = true)
+	Integer getBookPrice(Integer bookId);
 
 }
