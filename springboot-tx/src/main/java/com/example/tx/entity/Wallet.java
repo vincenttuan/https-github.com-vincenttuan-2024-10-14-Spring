b21f794374ffc27e443c47c10e80d87227a243bb
teacher <teacher@BE-403->
2024-12-16 19:12:32 +0800

    web

--- springboot-tx/src/main/java/com/example/tx/entity/Wallet.java
+++ springboot-tx/src/main/java/com/example/tx/entity/Wallet.java
@@ -1,5 +1,21 @@
 package com.example.tx.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Wallet {
+	
+	@Id
+	private String username; // 用戶名
+	@Column
+	private Integer balance; // 餘額
 
 }
